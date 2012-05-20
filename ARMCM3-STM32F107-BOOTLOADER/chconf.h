c886c685ff675d6f29361613fdc615e332a3a4ea
Matthias Blaicher <matthias@blaicher.com>
2012-05-20 08:25:45 +0200

    Deactivate unneeded ChibiOS features

--- ARMCM3-STM32F107-BOOTLOADER/chconf.h
+++ ARMCM3-STM32F107-BOOTLOADER/chconf.h
@@ -60,7 +60,7 @@
  *          and generally faster.
  */
 #if !defined(CH_TIME_QUANTUM) || defined(__DOXYGEN__)
-#define CH_TIME_QUANTUM                 20
+#define CH_TIME_QUANTUM                  0
 #endif
 
 /**
@@ -132,7 +132,7 @@
  * @note    The default is @p TRUE.
  */
 #if !defined(CH_USE_REGISTRY) || defined(__DOXYGEN__)
-#define CH_USE_REGISTRY                 TRUE
+#define CH_USE_REGISTRY                 FALSE
 #endif
 
 /**
@@ -143,7 +143,7 @@
  * @note    The default is @p TRUE.
  */
 #if !defined(CH_USE_WAITEXIT) || defined(__DOXYGEN__)
-#define CH_USE_WAITEXIT                 TRUE
+#define CH_USE_WAITEXIT                 FALSE
 #endif
 
 /**
